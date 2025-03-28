612225fd2a850ad200da2ce9487cbcd3ca836627
Marcelo.Monteiro <marcelo.monteiro@nosi.cv>
2025-03-28 10:24:27 -0100

    environment controlled paths [v. 0.0.1-alpha.95]

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,18 +1,12 @@
 import path from 'path';
 
-// Comment the following exports when building the application
-//export const CONFIGS = path.join(__dirname, '../../public/configs');
-//export const TEMPLATE_DIR = path.join(__dirname, '../../public/templates');
-//export const BASE_APP_ZIP = path.join(__dirname, '../../public/templates/base_app.zip');
-//export const BASE_WORKSPACE_ZIP = path.join(__dirname, '../../public/templates/base_workspace.zip');
-//export const PARTIALS_DIR = path.join(__dirname, '../../public/templates/components/{{name}}/partials');
+const isProductionEnv = process.env.ENGINE_IGRP_STUDIO_ENV === 'production'
 
-//Uncomment the following exports when building the application
-export const TEMPLATE_DIR = path.join(__dirname, './templates');
-export const CONFIGS = path.join(__dirname, './configs');
-export const BASE_APP_ZIP = path.join(__dirname, './templates/base_app.zip');
-export const BASE_WORKSPACE_ZIP = path.join(__dirname, './templates/base_workspace.zip');
-export const PARTIALS_DIR = path.join(__dirname, './templates/components/{{name}}/partials');
+export const CONFIGS = path.join(__dirname, isProductionEnv? './configs' : '../../public/configs');
+export const TEMPLATE_DIR = path.join(__dirname, isProductionEnv? './templates' : '../../public/templates');
+export const BASE_APP_ZIP = path.join(__dirname, isProductionEnv? './templates/base_app.zip' : '../../public/templates/base_app.zip');
+export const BASE_WORKSPACE_ZIP = path.join(__dirname, isProductionEnv? './templates/base_workspace.zip' : '../../public/templates/base_workspace.zip');
+export const PARTIALS_DIR = path.join(__dirname, isProductionEnv? './templates/components/{{name}}/partials' : '../../public/templates/components/{{name}}/partials');
 
 export const PATTERNS = {
   VALID_NAME_CONVENTIONAL: "^[a-zA-Z_]+$",
