287279ee158a2b96d2a4b88fdb4ae9f9ec0fd270
Marcelo.Monteiro <marcelo.monteiro@nosi.cv>
2025-11-24 10:46:32 -0100

    add items properties for card details component [v. 0.1.0-beta.40]

--- src/components/cardDetails/properties.ts
+++ src/components/cardDetails/properties.ts
@@ -7,9 +7,18 @@ import {
   commonPropertiesMapping,
 } from '../default/properties';
 import { INTERACTIONS_DEFAULTS, INTERACTIONS_TYPES } from '../../utils/constants';
+import { cardDetailsItemProperties } from './children/cardDetailsItem/properties';
 
 export function cardDetailsProperties() {
   return {
+    items: {
+      type: 'array',
+      items: {
+        ...cardDetailsItemProperties()
+      },
+      required: false,
+      'x-ui-widget': 'list'
+    },
     title: { type: 'string', required: false, default: 'Lorem Ipsum' },
     description: { type: 'string', required: false, default: 'Lorem ipsum dolor sit amet' },
     contentClassName: { type: 'string', required: false },
